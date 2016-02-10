be54ea0af4d74680415051ad7938fa3c2c44414b
Gregory BETTON <grebett@e3r4p1.42.fr>
2016-02-10 18:31:37 +0100

    setup: original commit

---
+++ mux.go
@@ -0,0 +1,40 @@
+//This package is a simple wrapper for the gorilla mux
+package mux
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// This struct contains the main router and a map of subrouters
+type Router struct {
+	Main *mux.Router
+	Subs map[string]*mux.Router
+}
+
+// This method returns a subrouter based on the provided prefix
+func (router *Router) NewSubrouter(prefix string) *mux.Router {
+	return router.Main.PathPrefix("/" + prefix).Subrouter()
+}
+
+// This function builds a new Router and return it
+func NewRouter() *Router {
+	return &Router{mux.NewRouter(), make(map[string]*mux.Router)}
+}
+
+// This middeware handles CORS
+func (r *Router) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", origin)
+		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		rw.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
+	}
+	// Lets Gorilla work (by calling the underneath ServeHTTP method
+	r.Main.ServeHTTP(rw, req)
+}

719b1e7de8b556e02cf1ccf64b38b02f1d74cd51
Gregory BETTON <grebett@e3r2p1.42.fr>
2016-02-11 15:04:58 +0100

    feat: Access-Control-Allow-Credentials

--- mux.go
+++ mux.go
@@ -30,6 +30,7 @@ func (r *Router) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		rw.Header().Set("Access-Control-Allow-Headers",
 			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		rw.Header().Set("Access-Control-Allow-Credentials", "true")
 	}
 	// Stop here if its Preflighted OPTIONS request
 	if req.Method == "OPTIONS" {
